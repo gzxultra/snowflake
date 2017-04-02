cc1a24d4f3318b2b96fcdc7b43bdb21af4083192
gzxultra <mygladfinger@gmail.com>
2017-04-02 19:04:34 +0800

    Add test cpp files for testing

---
+++ src/core/client.h
@@ -0,0 +1,30 @@
+#ifndef GREETER_CLIENT_H
+#define GREETER_CLIENT_H
+
+#include <memory>
+#include <string>
+#include <grpc++/grpc++.h>
+
+#include "src/proto/snowflake.pb.h"
+#include "src/proto/snowflake.grpc.pb.h"
+
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::Status;
+using snowflake::HelloRequest;
+using snowflake::HelloReply;
+using snowflake::Greeter;
+
+class GreeterClient {
+ public:
+  GreeterClient(std::shared_ptr<Channel> channel);
+  // Assambles the client's payload, sends it and presents the
+  // response back from the server.
+  std::string SayHello(const std::string& user);
+
+ private:
+  std::unique_ptr<Greeter::Stub> stub_;
+};
+
+#endif
+
